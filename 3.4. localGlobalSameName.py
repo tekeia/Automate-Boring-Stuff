c48fbb268ab50932f6e54711831464f127e271f7
Leonardo Borges <94leonardoborges@gmail.com>
2023-01-25 17:43:10 -0500

    global variables exercise addded

---
+++ 3.4. localGlobalSameName.py
@@ -0,0 +1,13 @@
+def spam():
+    eggs = 'spam local'
+    print(eggs)     #Prints 'spam local'
+
+def bacon():
+    eggs = 'bacon local'
+    print(eggs)     #Prints 'bacon local'
+    spam()
+    print(eggs)     #Prints 'bacon local'
+
+eggs = 'global'
+bacon()
+print(eggs)         #Prints 'global'
